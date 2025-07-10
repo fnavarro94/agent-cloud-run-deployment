d0fedadf7bfaf4eaffd2068779d950cb0aafe572
Felipe Navarro <felipenavarro@MacBook-Pro-de-Administrador.local>
2025-07-10 17:43:40 -0500

    cambie nombre a tipti client agent dev

--- tipti_client_agent_dev/__init__.py
+++ tipti_client_agent_dev/__init__.py
@@ -1,6 +1,6 @@
 """Tipti coordinator: provide reasonable decision strategies to select agents"""
 
 
-from tipti_client_agent.agent import root_agent
+from tipti_client_agent_dev.agent import root_agent
 
 __all__ = ["root_agent"]
